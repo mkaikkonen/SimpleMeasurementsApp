cd6d9bc86f22740e2c32fa200f5d1e18b5c3d209
Mika Kaikkonen <mika78_kaikkonen@hotmail.com>
2014-12-03 16:35:58 +0200

    Updates
    Added validations.

--- src/java/my/presentation/MeasurementView.java
+++ src/java/my/presentation/MeasurementView.java
@@ -24,13 +24,77 @@ public class MeasurementView {
     private MeasurementFacade measurementFacade;
     private Measurement measurement;   
     private String id;        
+    private String[] sensorArray = {"0", "0", "0", "0"};
+    private String note;
+    private String error;
+    
+    // Public properties
+    public String getErrorDescription() {
+        return error;
+    }
+    
+    public void setErrorDescription(String error) {
+        this.error = error;
+    }        
 
+    public Boolean getError() {
+        if( null != this.error){
+            return (this.error.length() > 0);
+        }
+        return false;
+    }
+
+    public void setError(Boolean set) {
+        if( false == set ){
+            this.error = "";
+        }  
+    }
+    
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getSensor1() {
+        return sensorArray[0];
+    }
+    public void setSensor1(String val) {
+        sensorArray[0] = val;
+    }
+    
+    public String getSensor2() {
+        return sensorArray[1];
+    }
+        
+    public void setSensor2(String val) {
+        sensorArray[1] = val;
+    }
+    
+    public String getSensor3() {
+        return sensorArray[2];
+    }
+    
+    public void setSensor3(String val) {
+        sensorArray[2] = val;
+    }
+
+    public String getSensor4() {
+        return sensorArray[2];
+    }
+    
+    public void setSensor4(String val) {
+        sensorArray[3] = val;
+    }
+    
+    public String getNote() {
+        return this.note;
+    }
+    
+    public void setNote(String note) {
+        this.note = note;
     }
     
     /**
@@ -59,10 +123,41 @@ public class MeasurementView {
     // Cerate measurement
     public String createMeasurement(){        
         System.out.println("Create measurement");
+
+        Float sensor1, sensor2, sensor3, sensor4;
+        
+        // Format checks
+        try{
+            sensor1 = Float.parseFloat(sensorArray[0]);
+            sensor2 = Float.parseFloat(sensorArray[1]);
+            sensor3 = Float.parseFloat(sensorArray[2]);
+            sensor4 = Float.parseFloat(sensorArray[3]);
+        } 
+        catch(NumberFormatException e){
+            System.out.println("Read measurement failed: " + e.getMessage());
+            this.setErrorDescription("Error: " + e.getMessage() );
+            return "createmeasurement";
+        }                
+        
+        // Boundary checks
+        if( ( sensor1 > 99.999 || sensor1 < -99.999 ) ||
+            ( sensor2 > 99.999 || sensor2 < -99.999 ) ||
+            ( sensor3 > 99.999 || sensor3 < -99.999 ) ||
+            ( sensor4 > 99.999 || sensor4 < -99.999 ) ||
+            note.length() > 50 )
+        {
+            this.setErrorDescription("Invalid value for field.");
+            return "createmeasurement";
+        }
         
         try{
             // Add date to new entry, this forces it to SQL CREATE
             measurement.setDate(new Date());
+            measurement.setSensor1(sensor1);
+            measurement.setSensor2(sensor2);
+            measurement.setSensor3(sensor3);
+            measurement.setSensor4(sensor4);
+            measurement.setNote(note);
             this.measurementFacade.create(measurement);
             System.out.println("Measurement created");
         }
@@ -78,12 +173,22 @@ public class MeasurementView {
     public String readMeasurement() {
         System.out.println("Read measurement: " + this.id);
 
+        int _id;
+        try{
+            _id = Integer.parseInt(this.id);
+        } 
+        catch(NumberFormatException e){
+            System.out.println("Read measurement failed: " + e.getMessage());
+            this.setErrorDescription("Error: " + e.getMessage() );
+            return "readmeasurement";
+        }
+        
         List<Measurement> msmnts = this.measurementFacade.findAll();
        
         for( Measurement meas : msmnts ) {
-            if( meas.getId() == Integer.parseInt(this.id) ) {
+            if( meas.getId() == _id ) {
                 measurement = meas;
-                System.out.println("Measurement " + this.id + " read");
+                System.out.println("Measurement " + _id + " read");
            }
         }
         return "readmeasurement";
@@ -93,37 +198,71 @@ public class MeasurementView {
     public String updateMeasurement(){       
         
         System.out.println("Update measurement " + this.id);
+        Float sensor1, sensor2, sensor3, sensor4;
+        int _id;
+        
+        // Format checks
+        try{
+            _id = Integer.parseInt(this.id);
+            sensor1 = Float.parseFloat(sensorArray[0]);
+            sensor2 = Float.parseFloat(sensorArray[1]);
+            sensor3 = Float.parseFloat(sensorArray[2]);
+            sensor4 = Float.parseFloat(sensorArray[3]);
+        } 
+        catch(NumberFormatException e){
+            System.out.println("Read measurement failed: " + e.getMessage());
+            this.setErrorDescription("Error: " + e.getMessage() );
+            return "updatemeasurement";
+        }                
+        
+        // Boundary checks
+        if( ( sensor1 > 99.999 || sensor1 < -99.999 ) ||
+            ( sensor2 > 99.999 || sensor2 < -99.999 ) ||
+            ( sensor3 > 99.999 || sensor3 < -99.999 ) ||
+            ( sensor4 > 99.999 || sensor4 < -99.999 ) )
+        {
+            this.setErrorDescription("Invalid value for field.");
+            return "updatemeasurement";
+        }
         
         List<Measurement> msmnts = this.measurementFacade.findAll();
        
         for( Measurement meas : msmnts ) {
-            if( meas.getId() == Integer.parseInt(this.id ) ) {
-                meas.setSensor1(measurement.getSensor1());
-                meas.setSensor2(measurement.getSensor2());
-                meas.setSensor3(measurement.getSensor3());
-                meas.setSensor4(measurement.getSensor4());
+            if( meas.getId() == _id ) {
+                meas.setSensor1(sensor1);
+                meas.setSensor2(sensor2);
+                meas.setSensor3(sensor3);
+                meas.setSensor4(sensor4);
                 meas.setNote(measurement.getNote());
                 this.measurementFacade.edit(meas);
-                System.out.println("Measurement " + this.id + " updated");
+                System.out.println("Measurement " + id + " updated");
            }          
        }
        return "updatemeasurement";
     }
     
     // Delete measurement
-    public String deleteMeasurement(){       
-       
+    public String deleteMeasurement(){              
         System.out.println("Delete measurement " + this.id);
         
+        int _id;
+        try{
+            _id = Integer.parseInt(this.id);
+        } 
+        catch(NumberFormatException e){
+            System.out.println("Delete measurement failed: " + e.getMessage());
+            this.setErrorDescription("Error: " + e.getMessage() );
+            return "deletemeasurement";
+        }
+        
         List<Measurement> msmnts = this.measurementFacade.findAll();
-       
-       for( Measurement meas : msmnts ) {
-           if( meas.getId() == Integer.parseInt(this.id ) ) {
-               System.out.println("Measurement " + this.id + " deleted");
-               this.measurementFacade.remove(meas);
-               }
-       }       
-       return "deletemeasurement";
+        
+        for( Measurement meas : msmnts ) {
+            if( meas.getId() == _id ) {
+                System.out.println("Measurement " + _id + " deleted");
+                this.measurementFacade.remove(meas);
+            }
+        }       
+        return "deletemeasurement";
     }
-
 }
